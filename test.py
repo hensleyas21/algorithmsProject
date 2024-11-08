ba219890c469eaecb293c02b87353e716c3a36fb
hensleyas21 <hensleyas21@gcc.edu>
2024-11-08 11:37:16 -0500

    Testing

---
+++ test.py
@@ -0,0 +1,11 @@
+import main
+import time
+
+# messing around with stuff
+
+if __name__ == '__main__':
+    a = main.random_list(8000000)
+    start_time = time.time()
+    lis = main.binary_search_lis(a)
+    runtime_naive = time.time() - start_time
+    print(f'\nBinary Search Solution:\n{lis}\nLength: {len(lis)}\n{runtime_naive:.2f} sec')
