2a4e78d7eba0c0ffb9e80de12f3341e20ed0cedd
hensleyas21 <hensleyas21@gcc.edu>
2024-11-09 15:06:43 -0500

    Added some comments

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import time
 import random
 
+# generates a list of length n filled with random integers [1, n]
 def random_list(n: int) -> list[int]:
     return [random.randint(1, n) for _ in range(n)]
 
+# the recursive step of the naive solution for finding longest increasing subsequence
+# this method should not be called directly, use naive_lis(arr) instead
 def naive_lis_recursive(arr: list[int]) -> list[int]:
     if len(arr) <= 1:
         return arr
@@ -21,20 +24,26 @@ def naive_lis_recursive(arr: list[int]) -> list[int]:
     
     return max_lis
 
+# finds the longest increasing subsequence using the naive solution
 def naive_lis(arr: list[int]) -> list[int]:
     max_lis = []
+
+    # calculates the LIS for arr[0:0], arr[0:1], arr[0:2], ... , arr[0:n] and set max_lis to the longest solution
     for i in range(len(arr)):
         lis_i = naive_lis_recursive(arr[:i+1])
         if len(max_lis) < len(lis_i):
             max_lis = lis_i
     return max_lis
 
+# finds the longest increasing subsequence using the binary tree solution
 def binary_search_lis(arr: list[int]) -> list[int]:
     if len(arr) == 0:
         return []
     
     lis = [arr[0]]
     for i in range(1, len(arr)):
+        # if the next value of arr is greater than the last value in the LIS, add it to the LIS.
+        # otherwise, use binary search to find where arr[i] goes in the LIS and substitute the existing value in the LIS with arr[i]
         if arr[i] > lis[-1]:
             lis.append(arr[i])
         else:
@@ -51,7 +60,7 @@ def binary_search_lis(arr: list[int]) -> list[int]:
 
 
 if __name__ == "__main__":
-    a = random_list(100)
+    a = random_list(50)
     print(f'Original List:\n{a}\nLength: {len(a)}')
 
     start_time = time.time()
