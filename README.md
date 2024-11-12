02b9de153f5c521ca174e8103c2902893397cda0
Liam Grossman <liam.j.grossman@gmail.com>
2024-11-11 21:22:55 -0500

    Add readme

---
+++ README.md
@@ -0,0 +1,20 @@
+# Run
+To run the tests, execute `python3 -m test.py` in your working directory
+
+# Logic
+## main.py
+`main.py` includes the logic for naive and efficient implementations for LIS (longest increasing subsequence). Comments are there to explain some of the logic.
+
+## test.py
+`test.py` includes logic for testing the implementations in `main.py`. It also used to run `main.py` and recieve the testing results.
+
+### Sequences
+The `Sequences` class is used to generate custom the following sequence types of size `n`:
+- Strictly increasing; `Sequences.strictly_increase`
+- Strictly decreasing; `Sequences.strickly_decrease`
+- Altnerating high/low: `Sequences.alternating_high_low`
+- Zig Zag: `Sequences.zig_zag`
+- Random: `Sequences.random`
+
+### main
+Executes the testing logic.
--- test.py
+++ test.py
@@ -26,10 +26,6 @@ class Sequences:
                 sequence.append(i)
         return sequence
 
-    @staticmethod
-    def constant(n: int = 100) -> list[int]:
-        constant = random.random()
-        return [constant for _ in range(1, n + 1)]
     
     @staticmethod
     def zig_zag(n: int = 100) -> list[int]:
@@ -42,46 +38,46 @@ class Sequences:
         return sequence
     
     @staticmethod
-    def repeated_pattern(n: int = 100) -> list[int]:
-        if n % 2 == 1:
-            pattern = [random.randint(1, 10) for _ in range(3)]
-            return (pattern * ((n // 3) + 1))[:n]
-        else:
-            pattern = [random.randint(1, 10) for _ in range(2)]
-            return (pattern * ((n // 2) + 1))[:n]
-
-    @staticmethod
     def random(n: int = 100) -> list[int]:
         return random.choices(range(1, n + 1), k=n)
 
 
+    def main():
+        
+        def run_test(generator, amount: list[int], verbose: bool = True):
+            seqs = [generator(n=n) for n in amount]
+            
+            for seq in seqs:
+                total_niave = 0
+                total_efficient = 0
+                print(f"Started {generator.__name__} for n={len(seq)}")
+
+                for _ in range(10):
+                    start_time = time.time()
+                    main.naive_lis(seq)
+                    total_time = (time.time() - start_time) * 1000
+                    total_niave += total_time
+                    if verbose: print(f"Naive solution for n={len(seq)} on {generator.__name__}: {total_time:.5f} ms")        
+                    start_time = time.time()
+                    main.binary_search_lis(seq)
+                    total_time = (time.time() - start_time) * 1000
+                    total_efficient += total_time
+                    if verbose: print(f"Efficient solution for n={len(seq)} on {generator.__name__}: {total_time:.5f} ms")    
+            
+                print(f"Naive solution avg for n={len(seq)} on {generator.__name__}: {(total_niave / 10):.5f} ms")                    
+                print(f"Efficient solution avg for n={len(seq)} on {generator.__name__}: {(total_efficient / 10):.5f} ms")        
+
+
+        t1 = multiprocessing.Process(target=run_test(Sequences.strictly_increase, [5, 10, 15, 20, 25], verbose=False))
+        t1.start()
+        t2 = multiprocessing.Process(target=run_test(Sequences.strictly_decrease, [5, 10, 15, 20, 25], verbose=False))
+        t2.start()
+        t3 = multiprocessing.Process(target=run_test(Sequences.alternating_high_low, [5, 10, 15, 20, 25, 30], verbose=False))
+        t3.start()
+        t4 = multiprocessing.Process(target=run_test(Sequences.zig_zag, [5, 10, 15, 20, 25], verbose=False))
+        t4.start()
+        t5 = multiprocessing.Process(target=run_test(Sequences.random, [5, 10, 15, 20, 25], verbose=False))
+        t5.start()  
+    
 if __name__ == '__main__':
-    # strictly increase test
-
-    def run_test(generator, amount: list[int]):
-        seqs = [generator(n=n) for n in amount]
-        for seq in seqs:
-            print(f"Started {generator.__name__} for n={len(seq)}")
-            start_time = time.time()
-            main.naive_lis(seq)
-            total_time = time.time() - start_time
-            print(f"Naive solution for n={len(seq)} on {generator.__name__}: {total_time} sec")        
-            start_time = time.time()
-            main.binary_search_lis(seq)
-            total_time = time.time() - start_time
-            print(f"Efficient solution for n={len(seq)} on {generator.__name__}: {total_time} sec")    
-
-    t1 = multiprocessing.Process(target=run_test(Sequences.strictly_increase, [10, 20, 30]))
-    t1.start()
-    t2 = multiprocessing.Process(target=run_test(Sequences.strictly_decrease, [10, 20, 30]))
-    t2.start()
-    t3 = multiprocessing.Process(target=run_test(Sequences.alternating_high_low, [10, 20, 30]))
-    t3.start()
-    t4 = multiprocessing.Process(target=run_test(Sequences.constant,[10, 20, 30]))
-    t4.start()
-    t5 = multiprocessing.Process(target=run_test(Sequences.zig_zag, [10, 20, 30]))
-    t5.start()
-    t6 = multiprocessing.Process(target=run_test(Sequences.repeated_pattern, [10, 20, 30]))
-    t6.start()
-    t7 = multiprocessing.Process(target=run_test(Sequences.random, [10, 20, 30]))
-    t7.start()
+    main()
